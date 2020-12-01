38b9e11da009fa48f974b8f9750640c39c37eeb5
Kwaku Gyapong <kwakugyapong219@gmail.com>
2020-12-01 14:45:57 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -65,7 +65,8 @@ def add_characters
 
 epic_tragedy[:additional_characters] = []
   
-
+epic_tragedy[:additional_characters] << { name: "Prince Escalus" }
+epic_tragedy[:additional_characters] << { name: "Apothecary"
 
 
 
